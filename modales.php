d855bb53683d286007d7a132a468119fe661a2f2
jlrodriguezalvarez <61981259+jlrodriguezalvarez@users.noreply.github.com>
2025-04-04 21:41:15 -0600

    Delete modales.php

--- modales.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Lista de modales disponibles
-$modales = ["modal_proyecto1", "modal_proyecto2"];
-
-// Cargar cada modal
-foreach ($modales as $modal) {
-    include("modales/$modal.php");
-}
-?>
